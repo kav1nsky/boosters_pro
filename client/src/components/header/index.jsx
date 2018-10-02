d72aa649f5b9112f5825a0014d3e43e288fd9058
Alena Kravets <alenka-kravec@mail.ru>
2018-10-02 11:14:39 +0300

    add header

--- client/src/components/header/index.jsx
+++ client/src/components/header/index.jsx
@@ -1,13 +1,38 @@
 import React from 'react';
 import styles from './styles.scss';
 
-
+import logo from '../../assets/media/logo.png';
 
 export class Header extends React.Component {
 
     render() {
         return <div className={styles.header}>
+            <div className={styles.headerContent}>
+                <img src={logo} alt='logo' className={styles.logoImg}/>
+                <div className={styles.headerTitle}>
+                    Boosters.pro
+                </div>
+                <div className={styles.headerNavigate}>
+                    <div className={styles.headerNavigateButton}>
+                        Чемпионаты
+                    </div>
+                    <div className={styles.headerNavigateButton}>
+                        Рейтинг
+                    </div>
+                    <div className={styles.headerNavigateButton}>
+                        О нас
+                    </div>
+                </div>
+                <div className={styles.registrationNavigate}>
+                    <div className={styles.registrationNavigateButton}>
+                        Вход
+                    </div>
+                    <div className={styles.registrationNavigateButton}>
+                        Регистрация
+                    </div>
+                </div>
 
+            </div>
         </div>
     }
 }
